1746828aaf39f1d9d1bfe4d72983ee8f4dc60b4c
reinforest915820 <alexandre915820@gmail.com>
2023-01-07 01:53:29 -0500

    [fix] changed the footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,12 +6,7 @@ import styles from './Footer.module.css';
 export const Footer = ({ className }) => (
   <footer className={classes(styles.footer, className)}>
     <Text size="s" align="center">
-      <span className={styles.date}>
-        {`© ${new Date().getFullYear()} Alexandre Andre.`}
-      </span>
-      <Link secondary className={styles.link} href="/humans.txt" target="_self">
-        Crafted by yours truly
-      </Link>
+      <span className={styles.date}>{`© ${new Date().getFullYear()} by Ylabs`}</span>
     </Text>
   </footer>
 );
