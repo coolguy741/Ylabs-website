86a0b23271561730d15c710a9ff457fac2919d03
reinforest915820 <alexandre915820@gmail.com>
2023-01-12 14:06:02 -0500

    [fix] restructure the slidesData

---
+++ src/pages/projects/projectsData.js
@@ -0,0 +1,20 @@
+export const slidesData = [
+  {
+    image: '/images/arufabudo/arufa_thumb_project.jpg',
+    title: 'Arufabudo',
+    meta: 'Premium, ready-made and custom-made boxing gear',
+    path: '/projects/arufabudo',
+  },
+  {
+    image: '/images/planify/image4.jpg',
+    title: 'Planifai',
+    meta: 'Comprehensive Business plan for 300$',
+    path: '/projects/planifai',
+  },
+  {
+    image: '/images/rapidcoder/image5.jpg',
+    title: 'Rapidcoder',
+    meta: 'Advance computer science courses',
+    path: '/projects/rapidcoder',
+  },
+];
